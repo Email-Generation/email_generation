6763fb9568b46684aedc8b9524c8506b530096a8
Kayvan Shah <shahkayvan28@gmail.com>
2023-08-06 16:20:38 -0700

    added services

---
+++ backend/app/services/openai_service.py
@@ -0,0 +1,29 @@
+import openai
+from app.config.settings import Settings
+from fastapi import HTTPException, status
+from openai import ChatCompletion
+
+# Load settings from the .env file
+settings = Settings()
+
+openai.organization = settings.OPENAI_ORGANIZATION
+openai.api_key = settings.OPENAI_API_KEY
+
+
+def generate_email(email_prompt: str) -> str:
+    req = ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": email_prompt}],
+    )
+
+    if req:
+        email_output = req.choices[0].message.content
+        # email_split = email_output.split("\n")
+        # for i in range(len(email_split)):
+        #     email_split[i] = "<p>" + email_split[i] + "</p>"
+        # email_output = "".join(email_split)
+        return email_output
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid Request"
+        )
