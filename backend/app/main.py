ba1a41db274fa9e1ca592d82ec741d3bd8e83dfd
Kayvan Shah <shahkayvan28@gmail.com>
2023-08-07 00:44:39 -0700

    patch for app as modeule

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(CURRENT_DIR))
+
 from app.config.settings import settings
 from app.routes.email_routes import router as email_router
 from fastapi import FastAPI
@@ -25,8 +31,6 @@ app = FastAPI(
     # license_info={"name": "BSD 3-Clause License", "url": "License URL"},
     lifespan=None,
 )
-handler = Mangum(app)
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -44,3 +48,5 @@ def root():
 
 # Add all of the routes to the application
 app.include_router(email_router, tags=["Emails"])
+
+handler = Mangum(app)
