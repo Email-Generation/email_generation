63ea4a91ad90323bc4e8e06dbf9740a7aaac8461
Kayvan Shah <shahkayvan28@gmail.com>
2023-08-06 18:33:33 -0700

    added logging settings

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,9 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    OPENAI_ORGANIZATION: str
-    OPENAI_API_KEY: str
+    OPENAI_ORGANIZATION: str = os.getenv("OPENAI_ORGANIZATION")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
     title: str = "Email Generation API"
     description: str = (
@@ -13,5 +18,15 @@ class Settings(BaseSettings):
         " communication with the Email Generation API."
     )
 
+    # Project Settings
+    APP_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR = os.path.dirname(APP_DIR)
+
+    DEFAULT_LOGGING_CONFIG_DIR = os.path.join(APP_DIR, "config", "logging")
+
     class Config:
         env_file = ".env"
+
+
+# Load settings from the .env file
+settings = Settings()
