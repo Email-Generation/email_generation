06d01f5329b987db4211c9af344857efb451a50a
Kayvan Shah <shahkayvan28@gmail.com>
2023-08-06 18:34:07 -0700

    added logger

--- backend/app/services/openai_service.py
+++ backend/app/services/openai_service.py
@@ -1,29 +1,38 @@
 import openai
-from app.config.settings import Settings
+from app.config.settings import settings
 from fastapi import HTTPException, status
 from openai import ChatCompletion
 
-# Load settings from the .env file
-settings = Settings()
+from app.config.logging import get_logger
+
+
+logger = get_logger()
 
 openai.organization = settings.OPENAI_ORGANIZATION
 openai.api_key = settings.OPENAI_API_KEY
 
 
 def generate_email(email_prompt: str) -> str:
-    req = ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": email_prompt}],
-    )
+    try:
+        req = ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": email_prompt}],
+        )
 
-    if req:
-        email_output = req.choices[0].message.content
-        # email_split = email_output.split("\n")
-        # for i in range(len(email_split)):
-        #     email_split[i] = "<p>" + email_split[i] + "</p>"
-        # email_output = "".join(email_split)
-        return email_output
-    else:
+        if req:
+            email_output = req.choices[0].message.content
+            # email_split = email_output.split("\n")
+            # for i in range(len(email_split)):
+            #     email_split[i] = "<p>" + email_split[i] + "</p>"
+            # email_output = "".join(email_split)
+            return email_output
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Invalid Request",
+            )
+    except Exception as e:
+        logger.exception(e)
         raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid Request"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Out of service"
         )
