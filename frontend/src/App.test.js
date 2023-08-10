be30a96acca27b1326c6342d81d529d875cc6196
Kayvan Shah <shahkayvan28@gmail.com>
2023-08-10 14:31:09 -0700

    updated the render tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header Text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Auto generate personalised email sequences/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders textarea placeholder', () => {
+  render(<App />);
+  const textareaElement = screen.getByPlaceholderText(/Write the email prompt/i);
+  expect(textareaElement).toBeInTheDocument();
+});
+
+test('updates email prompt on textarea change', () => {
+  render(<App />);
+  const textareaElement = screen.getByPlaceholderText(/Write the email prompt/i);
+  
+  fireEvent.change(textareaElement, { target: { value: 'New email prompt' } });
+  
+  expect(textareaElement.value).toBe('New email prompt');
 });
